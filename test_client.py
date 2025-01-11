5513ba6724286a319b471d90e33ea63640ef117d
jaesung <dlawo411@gmail.com>
2025-01-11 20:09:39 +0900

    edit test client

--- test_client.py
+++ test_client.py
@@ -1,13 +1,11 @@
 from app import app  # Flask 앱 임포트
-
-def test_checkout():
+import random
+import time
+def test_checkout(cart_items):
     client = app.test_client()  # Flask 테스트 클라이언트 생성
     with client.session_transaction() as session:
         session['login_info'] = {'UserID': 'test_user'}  # 세션 데이터 설정
-    
-    cart_items = [
-       {'product_name': 'test2', 'price': 2000, 'quantity': 1, 'image_path': 'https://imagebucketmini3.s3.ap-northeast-3.amazonaws.com/test2/s4.jpeg'}
-    ]
+
     response = client.post('/bucket/checkout', data={
         'cart_items': str(cart_items)
     })
@@ -15,8 +13,25 @@ def test_checkout():
     print("Response data:", response.data.decode())
     assert response.status_code == 302  # 리다이렉트 성공 여부 확인
 
+items = [
+        [
+            {'product_name': '맥북', 'price': 12000, 'quantity': 1, 'image_path': 'https://imagebucketmini3.s3.ap-northeast-3.amazonaws.com/test2/s4.jpeg'}
+        ],
+        [
+            {'product_name': '이지스함', 'price': 150000, 'quantity': 1, 'image_path': 'https://imagebucketmini3.s3.ap-northeast-3.amazonaws.com/test2/s4.jpeg'}
+        ],
+        [
+            {'product_name': '신세계상품권', 'price': 100000, 'quantity': 1, 'image_path': 'https://imagebucketmini3.s3.ap-northeast-3.amazonaws.com/test2/s4.jpeg'}
+        ],
+        [
+            {'product_name': '신발', 'price': 50000, 'quantity': 1, 'image_path': 'https://imagebucketmini3.s3.ap-northeast-3.amazonaws.com/test2/s4.jpeg'}
+        ]
+    ]
+
 # 판매 데이터 샘플 전송
 if __name__ == "__main__":
-    for i in range(100):
+
+    while True:
         # 테스트 실행
-        test_checkout()
+        test_checkout(items[random.randint(0, 3)])
+        time.sleep(3)
